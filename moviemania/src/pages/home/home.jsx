026456fcad50b7ede5032849be5f23d49fac2abc
Tanmay Mahajan <92631626+tanmaym23@users.noreply.github.com>
2024-08-22 09:48:11 +0530

    Update home.jsx

--- moviemania/src/pages/home/home.jsx
+++ moviemania/src/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import MovieList from "../../components/MovieList/movie-list";
 import Slider from "../../components/slider/slider";
-
+import "./Home.css";
 const Home = () => {
   return (
     <div>
